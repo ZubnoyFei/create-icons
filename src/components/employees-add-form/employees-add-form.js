16a34694dff684d34f172d4eca298a6e6d515e3e
Your Name <ustinovoleksij@gmail.com>
2022-05-26 18:25:49 +0300

    Added filters

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 
-import './employees-add-form.css';
-
+//import './employees-add-form.css';
+import './employee-add-form.scss';
 class EmployeesAddForm extends Component {
     constructor(props) {
         super(props);
