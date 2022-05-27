3e62f435a010d5a79ebdab20bcd8f3cbb9734726
Your Name <ustinovoleksij@gmail.com>
2022-05-27 10:30:59 +0300

    Added slider bootstrap

--- src/bootstrapTest.js
+++ src/bootstrapTest.js
@@ -1,11 +1,29 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Form, Button} from 'react-bootstrap';
 
 const bootstrapTest = () => {
     return (
         <Container>
             <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
+                <Col><Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"/>
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form></Col>
             </Row>
         </Container>
     )
