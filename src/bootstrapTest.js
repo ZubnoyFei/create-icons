074f5917a3e2c03a22c28505f3bc8cf260789345
Your Name <ustinovoleksij@gmail.com>
2022-05-27 09:15:58 +0300

    added bootstrap

---
+++ src/bootstrapTest.js
@@ -0,0 +1,14 @@
+import {Container, Row, Col} from 'react-bootstrap';
+
+const bootstrapTest = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default bootstrapTest();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-
+import bootstrapTest from "./bootstrapTest";
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
+
     document.getElementById('root')
 );
